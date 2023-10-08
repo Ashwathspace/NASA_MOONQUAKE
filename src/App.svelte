e032360582963e7132d5e633b66ef1bf4b3280a5
Palaniappan Subramanian <147251106+Ashwathspace@users.noreply.github.com>
2023-10-08 19:41:26 +0200

    App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 	let rotating = true;
 	let cheese = false;
 	$: legend = [
-		{ index: 0, name: "Moonquake", color: "#000000", display: true, opacity: 1},
+		{ index: 0, name: "Moonquake", color: "#006635", display: true, opacity: 1},
 		{ index: 1, name: "Mapped Craters", color: "#5E17EB", display: true, opacity: 1},
 		{ index: 2, name: "Moon Caves", color: "#0083C2", display: true, opacity: 1},
 		{ index: 3, name: "Apollo Landings", color: "#FF914D", display: true, opacity: 1}
